11a3b9363931c96a019a72f6f1f015521eec619b
LoginovskyMax <frakiec3600@gmail.com>
2023-06-23 19:08:17 +0600

    feat:add main page, not found

---
+++ src/Pages/MainPage/index.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import AuthenticationModal from "../../Components/Authentication";
+import Button from "../../Components/common/Button";
+import useUserStore from "../../store";
+import languageStore from "../../store/language";
+import themeStore from "../../store/theme";
+
+import "./style.scss";
+
+const MainPage = () => {
+  const [isModalClosed, setModalClosed] = useState(true);
+  const navigate = useNavigate();
+  const user = useUserStore((state) => state.userName);
+  const theme = themeStore((state) => state.isDark);
+  const isEn = languageStore((state) => state.isEn);
+
+  const handler = () => {
+    if (user === null) {
+      setModalClosed(false);
+    } else {
+      navigate("/games");
+    }
+  };
+
+  return (
+    <div className={theme ? "main-page dark-theme" : "main-page"}>
+      <img
+        alt="Online games players"
+        src="images/main.png"
+        className="main-page__photo"
+      />
+      <p className="main-page__main-text">
+        {isEn
+          ? "Начни играть вместе с Gaming Zone"
+          : "Let`s play with Gaming Zone"}
+      </p>
+      <p className="main-page__secondary-text">
+        {isEn
+          ? `Gaming Zone один из лучших сайтов`
+          : `Gaming Zone has the best free site`}
+      </p>
+      <Button className="main-page__button" onClick={handler}>
+        {isEn ? "Начать" : "Get started"}
+      </Button>
+      {!isModalClosed && (
+        <AuthenticationModal setModalClosed={() => setModalClosed(true)} />
+      )}
+    </div>
+  );
+};
+
+export default MainPage;
